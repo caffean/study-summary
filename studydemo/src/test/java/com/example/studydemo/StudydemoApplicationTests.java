266484e43f5cede1a7b2cf106d2846712e03f008
lyn <m15754716463@163.com>
2019-08-23 09:53:25 +0800

    第一次提交

---
+++ studydemo/src/test/java/com/example/studydemo/StudydemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.studydemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudydemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
